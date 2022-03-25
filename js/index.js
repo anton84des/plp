ecbf32b74167bee68fb9179dd04503f760c48d32
Kucher Artem <kucher.artem97@gmail.com>
2022-03-26 01:52:14 +0200

    Task 7-1
    Added reset function, corrected some styling

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 let productItems = document.querySelectorAll('.products-item');
+let colorCircles = document.querySelectorAll('.color-filter-label');
 
 //function that adds classes to product items regarding available colors
-function colorsAssign () {
+function colorsAssign() {
     for (let item of productItems) {
        let infoColorItems = item.querySelectorAll('.products-info-color-item');
 
@@ -14,10 +15,22 @@ function colorsAssign () {
 
 }
 
-//function that filtrates products by color
-function colorFilter () {
-    let colorCircles = document.querySelectorAll('.color-filter-label');
+//resets chosen filter
+function resetFilter(filter, circleColor) {
+    let input = filter.previousElementSibling;
 
+    input.checked = false;
+
+    for (let item of productItems) {
+        if (!item.classList.contains(circleColor)) {
+            item.classList.remove('d-none');
+        }
+    }
+
+}
+
+//function that filtrates products by color
+function colorFilter() {
     colorCircles.forEach(circle => {
         circle.onclick = function () {
             let circleColor = this.getAttribute('data-color');
@@ -26,6 +39,13 @@ function colorFilter () {
                 if (!item.classList.contains(circleColor)) {
                     item.classList.add('d-none');
                 }
+            }
+
+            if (this.classList.contains('js-chosen-filter')) {
+                this.classList.remove('js-chosen-filter');
+                resetFilter(this, circleColor);
+            } else {
+                this.classList.add('js-chosen-filter');
             }
         }
     });
