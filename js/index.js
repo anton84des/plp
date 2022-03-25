f05f92bbf2790f98ab839a16449a2941f928a063
Kucher Artem <kucher.artem97@gmail.com>
2022-03-26 01:18:49 +0200

    Task 7-1
    -Added colorFilter function

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
+let productItems = document.querySelectorAll('.products-item');
+
 //function that adds classes to product items regarding available colors
 function colorsAssign () {
-    let productItems = document.querySelectorAll('.products-item');
-
     for (let item of productItems) {
        let infoColorItems = item.querySelectorAll('.products-info-color-item');
 
@@ -15,3 +15,22 @@ function colorsAssign () {
 }
 
 colorsAssign();
+
+//function that filtrates products by color
+function colorFilter () {
+    let colorCircles = document.querySelectorAll('.color-filter-label');
+
+    colorCircles.forEach(circle => {
+        circle.onclick = function () {
+            let circleColor = this.getAttribute('data-color');
+
+            for (let item of productItems) {
+                if (!item.classList.contains(circleColor)) {
+                    item.classList.add('d-none');
+                }
+            }
+        }
+    });
+}
+
+colorFilter();
