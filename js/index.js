c3f1feaac7a1f2ede37d606139318313bda989e5
Kucher Artem <kucher.artem97@gmail.com>
2022-04-01 01:58:05 +0300

    ticket-7-1
    fixed bug for filter combination

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
+let filters = document.querySelector('.filter');
 let productItems = document.querySelectorAll('.products-item');
-let colorCircles = document.querySelectorAll('.color-filter-label');
 
 //function that adds classes to product items regarding available colors
 function colorsAssign() {
@@ -15,41 +15,26 @@ function colorsAssign() {
 
 }
 
-//resets chosen filter
-function resetFilter(filter, circleColor) {
-    let input = filter.previousElementSibling;
-
-    input.checked = false;
-
-    for (let item of productItems) {
-        if (!item.classList.contains(circleColor)) {
-            item.classList.remove('d-none');
-        }
-    }
-
-}
-
 //function that filtrates products by color
 function colorFilter() {
-    colorCircles.forEach(circle => {
-        circle.onclick = function () {
-            let circleColor = this.getAttribute('data-color');
+    let colorCircles = document.querySelectorAll('input:checked');
 
-            for (let item of productItems) {
-                if (!item.classList.contains(circleColor)) {
-                    item.classList.add('d-none');
-                }
-            }
+    for (item of productItems) {
+        item.classList.add('d-none');
 
-            if (this.classList.contains('js-chosen-filter')) {
-                this.classList.remove('js-chosen-filter');
-                resetFilter(this, circleColor);
-            } else {
-                this.classList.add('js-chosen-filter');
+        if (colorCircles.length === 0) {
+            item.classList.remove('d-none');
+        }
+
+        for (circle of colorCircles) {
+            let circleColor = circle.getAttribute('data-color');
+
+            if (item.classList.contains(circleColor)) {
+                item.classList.remove('d-none');
             }
         }
-    });
+    }
 }
 
 colorsAssign();
-colorFilter();
+filters.addEventListener('input', colorFilter)
